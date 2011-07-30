2c168882e46e318c89e8b3d180b099f45f277329
Oleg Puzanov <puzanov@gmail.com>
2011-07-30 22:57:44 +0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dedman::Application.config.secret_token = '526b654cede5c55514f6770365ec9a45f6fb06e531ee2a347f805ee0aba935676fc7a6c00758636cb3d9062673c525cd3126c6be05e222d22c1526b7c47a385c'
